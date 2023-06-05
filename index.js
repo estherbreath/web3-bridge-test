d3d41d2a8e26723c19a7a4a18b317be2e648e72a
Esther Breath <estherojile248@gmail.com>
2023-06-05 12:57:38 +0000

    feat: password validation

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const country_err = document.getElementById("country_err");
 const email_err = document.getElementById("email_err");
 const password_err = document.getElementById("password_err");
 
+const elements = [
+  { element: firstname, err: firstname_err },
+  { element: lastname, err: lastname_err },
+  { element: country, err: country_err },
+  { element: email, err: email_err },
+  { element: password, err: password_err },
+];
+
+for(let i = 0; i < elements.length; i++){
+    const content = elements[i]
+    content.element.addEventListener(('input'), () => {
+        content.err.innerHTML = ""
+    })
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (validateForm()) {
@@ -26,8 +41,15 @@ function validateForm() {
   );
   const lastnameValidate = validateString(lastname, lastname_err, "Lastname");
   const countryValidate = validateString(country, country_err, "Location");
-  const emailValidate = validateEmail(email, email_err, 'Email')
-  if (firstnameValidate && lastnameValidate && countryValidate && emailValidate) {
+  const emailValidate = validateEmail(email, email_err, "Email");
+  const passwordValidate = validatePassword(password, password_err, "Password");
+  if (
+    firstnameValidate &&
+    lastnameValidate &&
+    countryValidate &&
+    emailValidate &&
+    passwordValidate
+  ) {
     return true;
   } else {
     return false;
@@ -47,10 +69,22 @@ function validateEmail(element, errElement, fieldname) {
   const validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if(!element.value.match(validRegex)){
-        errElement.innerHTML = `${fieldname} is required`
-        return false
-    }else {
-        return true
-    }
+  if (!element.value.match(validRegex)) {
+    errElement.innerHTML = `${fieldname} is required`;
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validatePassword(element, errElement, fieldname) {
+  if (password.value === "") {
+    errElement.innerHTML = `${fieldname} is required and should be greater then 8`;
+    return false;
+  } else if (element.value.length < 8) {
+    errElement.innerHTML = `${fieldname} should be greater then 8`;
+    return false;
+  } else {
+    return true;
+  }
 }
