b86a686099a41c477e9030649babcc0ac59414d4
Esther Breath <estherojile248@gmail.com>
2023-06-05 12:13:05 +0000

    feat: get access to dom elememt

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+const form = document.getElementById('form')
+const firstname = document.getElementById('firstname')
+const lastname = document.getElementById('lastname')
+const country = document.getElementById('country')
+const email = document.getElementById('email')
+const password = document.getElementById("password")
+
+const firstname_err = document.getElementById('firstname_err')
+const lastname_err = document.getElementById('lastname_err')
+const country_err = document.getElementById('country_err')
+const email_err = document.getElementById('email_err')
+const password_err = document.getElementById("password_err")
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+})
